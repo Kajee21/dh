9012a9ac36cadfaabddefe744e5d114cd474c31a
Kajee21 <108038004+Kajee21@users.noreply.github.com>
2023-08-20 20:20:17 +0530

    about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -19,8 +19,8 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
+        After the training in{" "}
+        <span className="font-medium">Web development</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
@@ -41,8 +41,7 @@ export default function About() {
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">history and Science</span>.
       </p>
     </motion.section>
   );
